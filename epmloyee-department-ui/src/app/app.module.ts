f92820c09529c2be7c348a67609f0b6fd4f77ff6
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-10-30 18:16:40 +0200

    dfdf

---
+++ epmloyee-department-ui/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DepartmentComponent } from './department/department.component';
+import { ShowDepartmentComponent } from './department/show-department/show-department.component';
+import { AddEditDepartmentComponent } from './department/add-edit-department/add-edit-department.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { ShowEmployeeComponent } from './employee/show-employee/show-employee.component';
+import { AddEditEmployeeComponent } from './employee/add-edit-employee/add-edit-employee.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DepartmentComponent,
+    ShowDepartmentComponent,
+    AddEditDepartmentComponent,
+    EmployeeComponent,
+    ShowEmployeeComponent,
+    AddEditEmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
