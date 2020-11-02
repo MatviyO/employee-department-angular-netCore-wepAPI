ac1bf0424aa6fe4d0459313e90f507e114f5ad16
Oleh <33325458+MatviyO@users.noreply.github.com>
2020-11-02 12:30:02 +0200

    up

--- epmloyee-department-ui/src/app/employee/add-edit-employee/add-edit-employee.component.ts
+++ epmloyee-department-ui/src/app/employee/add-edit-employee/add-edit-employee.component.ts
@@ -10,17 +10,37 @@ export class AddEditEmployeeComponent implements OnInit {
   @Input() dep: any;
   EmployeeId: string;
   EmployeeName: string;
+  Department: string;
+  DateOfJoining: string;
+  PhotoFileName: string;
+  PhotoFilePath: string;
+
+  DepartmentList: any[] = [];
 
   constructor(private service: SharedService) { }
 
   ngOnInit(): void {
-    this.EmployeeId = this.dep.EmployeeId;
-    this.EmployeeName = this.dep.EmployeeName;
+    this.loadDepartmentList();
   }
+
+  loadDepartmentList(): any {
+    this.service.getAllDepartmentNames().subscribe(res => {
+      this.DepartmentList = res;
+      this.EmployeeId = this.dep.EmployeeId;
+      this.EmployeeName = this.dep.EmployeeName;
+      this.Department = this.dep.Department;
+      this.DateOfJoining = this.dep.DateOfJoining;
+      this.PhotoFileName = this.dep.PhotoFileName;
+      this.PhotoFilePath = this.service.PhotoUrl + this.PhotoFileName;
+    });
+  }
+
   addEmp(): any {
     const value = {
       DepartmentId: this.EmployeeId,
-      DepartmentName: this.EmployeeName
+      DepartmentName: this.EmployeeName,
+      Department: this.Department,
+      DateOfJoining: this.DateOfJoining
     };
     this.service.addEmployee(value).subscribe(res => {
       alert(res.toString());
@@ -34,6 +54,15 @@ export class AddEditEmployeeComponent implements OnInit {
     };
     this.service.updateEmployee(value).subscribe(res => {
       alert(res.toString());
+    });
+  }
+  uploadPhoto(event): any {
+    const file = event.target.files[0];
+    const formData: FormData = new FormData();
+    formData.append('uploadFile', file, file.name);
+    this.service.UploadPhoto(formData).subscribe(res => {
+      this.PhotoFileName = res.toString();
+      this.PhotoFilePath = this.service.PhotoUrl + this.PhotoFileName;
     });
   }
 
